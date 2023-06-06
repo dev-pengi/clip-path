44088df1091547fb5868f50a0238d017ae33f0c6
dev-pengi <siffffdoul@gmail.com>
2023-06-06 20:57:20 +0100

    Update the styles

--- src/styles.ts
+++ src/styles.ts
@@ -1,9 +1,7 @@
 const contentPadding: string = `lg:px-[90px] md:px-[60px] sm:px-[40px] vsm:px-[15px] px-[8px] `;
 
-const gardient: string = "bg-gradient-to-r from-primary to-secondary";
-const buttonStyle: string = `${gardient} w-max px-4 py-3 text-[14px] rounded-[5px] cursor-pointer duration-300 hover:scale-[0.95]`;
 
 const colorbuttonStyle: string = `min-w-[35px] w-[35px] min-h-[35px] h-[35px] rounded-full cursor-pointer duration-300 shadow-lg hover:shadow-strong hover:scale-[1.1]`;
 const optionbuttonStyle: string = `px-5 py-2 shadow-md flex items-center justify-center gap-2 rounded-md duration-300 hover:shadow-strong hover:-translate-y-[4px]`;
 
-export { contentPadding, gardient, buttonStyle, colorbuttonStyle, optionbuttonStyle };
+export { contentPadding, colorbuttonStyle, optionbuttonStyle };
